d7a8b81f8e2b16f6b5f8bb5021f5c8efe163b0bb
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-06-25 19:31:03 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const aps = express();
 
 aps.set('port', '8000');
-app.set('view engine', 'ejs');
+aps.set('view engine', 'ejs');
 
 aps.use(require('./routes/index_route'));
-
-
 
 aps.listen(aps.get('port'), function () {
     console.log('Example app listening on port', aps.get('port'));
--- src/routes/index_route.js
+++ src/routes/index_route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const route = express.Router();
 
 route.get('/', function (req, res) {
-    res.send('el coso funciona');
+    res.render('index.ejs');
 });
 
 module.exports = route;
